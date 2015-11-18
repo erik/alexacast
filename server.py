e40ec8c6607108bd6bf705f16b4aefbacf6526d5
Erik Price <github@erikprice.net>
2015-11-17 20:33:11 -0800

    Rewrite using alexandra lib

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import sys
 
+import alexandra
 import click
-import flask
 import pychromecast
 
 
 device_name = None
 cast = None
-app = flask.Flask(__name__)
+app = alexandra.Application()
 
 
 @click.command()
@@ -27,99 +27,68 @@ def server(device):
     print(repr(cast))
     print('connected, starting up...')
 
-    app.run('0.0.0.0', 8183, debug=True)
+    app.run_debug('0.0.0.0', 8183)
 
 
-@app.route('/', methods=['POST'])
-def dispatch_request():
-    body = flask.request.get_json()
-    print(body)
-
-    req = body['request']
-
-    if req['type'] != 'IntentRequest':
-        return 'nope', 400
-
-    intent_handler = {
-        'SkipMedia': skip_media,
-        'PlayMedia': play_media,
-        'PauseMedia': pause_media,
-        'Reconnect': reconnect,
-        'Reboot': reboot
-    }.get(req['intent']['name'])
-
-    if intent_handler:
-        return intent_handler()
-
-    return 'NO.', 400
-
-
-def _format_response(message):
-    body = {
-        'version': '1.0',
-        'sessionAttributes': {},
-        'response': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': message
-            },
-
-            'shouldEndSession': True
-        }
-    }
-
-    return flask.jsonify(**body)
-
-
-# TODO: at some point should probably not have this return 200 OK
-def _error(message=''):
-    return _format_response(message)
-
-
-def _success(message=''):
-    return _format_response(message)
-
-
-def reconnect():
+@app.intent('Reconnect')
+def reconnect(slots, session):
     global cast
 
     cast = pychromecast.get_chromecast(friendly_name=device_name)
-    if not cast:
-        return _error(
-            'Failed to connect to Chromecast named %s.' % device_name
-        )
 
-    return _success('Reconnected.')
+    if cast is None:
+        return alexandra.respond(
+            text='Failed to connect to Chromecast named %s.' % device_name)
+
+    return alexandra.respond(text='Reconnected.')
 
 
-def skip_media():
+@app.intent('SeekMedia')
+def seek_media(slots, session):
+    """TODO """
+    mc = cast.media_controller
+
+    mc.seek(mc.pos)
+    return alexandra.respond(text='TODO')
+
+
+@app.intent('SkipMedia')
+def skip_media(slots, session):
     mc = cast.media_controller
 
     if not mc.status.supports_skip_forward:
-        return _error("Skipping not supported")
+        return alexandra.respond(text="Skipping not supported")
 
     mc.skip()
-    return _success()
+    return alexandra.respond()
 
 
-def play_media():
+@app.intent('PlayMedia')
+def play_media(slots, session):
     mc = cast.media_controller
 
     if mc.status.player_is_playing:
-        return _error("Already playing")
+        return alexandra.respond(text="Already playing")
 
     mc.play()
-    return _success()
+    return alexandra.respond()
 
 
-def pause_media():
-    cast.media_controller.pause()
-    return _success()
+@app.intent('PauseMedia')
+def pause_media(slots, session):
+    mc = cast.media_controller
+
+    if not mc.status.player_is_playing:
+        return alexandra.respond(text="Already paused")
+
+    mc.pause()
+    return alexandra.respond()
 
 
-def reboot():
+@app.intent('Reboot')
+def reboot(slots, session):
     cast.reboot()
-    return _success()
+    return alexandra.respond()
 
 
 if __name__ == '__main__':
